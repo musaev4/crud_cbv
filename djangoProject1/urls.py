3c4e20e647bed688936fe2c927e9dc15d7fd69b3
Ibragim <musaev4@outlook.com>
2024-04-04 22:26:02 +0600

    The main route has been chance

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('', lambda r: redirect('/book/'), name='main')
 ]
